aac8750f46f1f8e61eaf3bf46779542f8ba3a3d2
Miguel Silva <miguel.oliveira.silva@ist.utl.pt>
2017-10-23 19:02:52 +0100

    is not working yet

--- update_M.m
+++ update_M.m
@@ -1,6 +1,7 @@
 function M=update_M(U_prev,X,O_prev,q,C)
     M=zeros(2,C);
-    for i=1:C
-        M(:,i)=sum(((U_prev(:,i)^q).*(X-O_prev))/sum(U_prev(:,i)^q));
+    for c=1:C
+        %equacao 9
+        M(:,c)=sum(((U_prev(:,c).^q).*(X-O_prev'))/sum(U_prev(:,c).^q));
     end
 end
--- update_U.m
+++ update_U.m
@@ -1,8 +1,11 @@
-function U=update_U(X,M,O,lambda,q,C)
-    U=zeros(size(X,1),C);
+function U=update_U(X,M,O,lambda,q,C,N)
+    U=zeros(N,C);
     %Hard K-means
     if lambda==1
-        U(:,min(norm(X-M-O)))=1;
+        for i=1:N
+            [~,c_min]=min(norm(repmat(X(i,:),[C,1])-M'-O(:,i)'));
+            U(i,c_min)=1;
+        end
     %Soft K-means
     else
         ;
