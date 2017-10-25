3fe4be03f31af604e606f27ea34fbb88aabd47d8
Miguel Silva <miguel.oliveira.silva@ist.utl.pt>
2017-10-25 08:48:16 +0100

    not working YET

--- update_O.m
+++ update_O.m
@@ -3,13 +3,15 @@ function O=update_O(X,M,U_prev,lambda,q,C,N)
     r=zeros(2,N);
     for i=1:N
         %equacao 12
+        D=zeros(1,C)
+        N=
         for c=1:C
-            D=U_prev(i,:).^q
-            N=(U_prev(i,:).^q)*(X(i,:)-M')
+            D=D+U_prev(i,:).^q
+            N=N+(U_prev(i,:).^q)*(X(i,:)-M')
             %r(i,:)=sum((U_prev(i,:).^q).*(X(i,:)-M'),2)/sum(U_prev(i,:).^q,2);
         end
-        r=sum(N,2)/sum(D,2)
+        r=N/D
         %equacao 11
-        O(i,:)=r(i,:)*(1-lambda/(2*norm(r(i,:))));
+        O(:,i)=r(:,i)*(1-lambda/(2*norm(r(i,:))));
     end
 end
