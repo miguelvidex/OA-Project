93701fbaea7532c2a9306f5ebfc43e76cc277f5c
pedrofcsousa <pedrofcsousa@tecnico.ulisboa.pt>
2017-10-31 11:06:53 +0000

    Adding soft k-means formula

--- plot_it.m
+++ plot_it.m
@@ -5,6 +5,7 @@ function plot_it(M,O,X,U,N,t)
    P=0;
    m_plot=plot(M(1,:),M(2,:),'ok');
    for i=1:N
+       
        if O(:,i)~=0
             plot(X(i,1),X(i,2),'.r');
        elseif U(i,1)== 1 
