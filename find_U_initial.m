f83a449727d6b5e1d7dabd525e0d18e512a23322
Miguel Silva <miguel.oliveira.silva@ist.utl.pt>
2017-11-20 21:33:36 +0000

    new file to create initial U

---
+++ find_U_initial.m
@@ -0,0 +1,52 @@
+close all
+clear all
+load('dataset/dataset_project6_clustering.mat')
+%print flag
+print_flag=0;
+%number of elements in dataset
+N;
+%Input data matrix X
+X=[dataset(:,1),dataset(:,2)];
+%number of clusters C
+C=N_clusters;
+%q = 1 implies hard K-means
+q_hard=1;
+q_soft=1.5;
+%
+lambda=10;
+%matrix with outliers(initialize it without outliers)
+O_init=zeros(2,N);
+%Solutions are saved in U (initialize it)
+%U{1}=initU(X,N,C);
+U_init=initU_randomly(N,C);
+
+%threshold to exit the 
+threshold=10^(-3);
+
+M_init=zeros(2,C);
+
+N_points=30;
+n_outliers=zeros(1,N_points);
+loglambda=logspace(-2,3,N_points);
+best_U_indice=1;
+for log_lambda_i=1:N_points
+    [M_aux, O_aux, U_aux]=Algorithm_1(M_init,O_init,X,U_init,N,C,loglambda(log_lambda_i),q_hard,threshold,print_flag);
+    if any(isnan(M_aux(:)))
+        n_outliers(1,log_lambda_i)=nan;
+    else
+        n_outliers(1,log_lambda_i)= sum(O_aux(1,:)~=0 & O_aux(2,:)~=0,2);
+        if abs(n_outliers(1,log_lambda_i)-80)< abs(n_outliers(1,best_U_indice)-80)
+            best_U = U_aux;
+            best_U_indice=log_lambda_i;
+        end
+    end
+end
+figure(10)
+semilogx(loglambda,n_outliers,'-s')
+grid on
+title('Numero de outliers no varrimento no \lambda')
+xlabel('log(\lambda)') % x-axis label
+ylabel('Numero de outliers') % y-axis label
+
+U_init=best_U;
+save('Init_variables.mat', 'U_init')
--- part1.m
+++ part1.m
@@ -3,8 +3,6 @@ close all
 load('dataset/dataset_project6_clustering.mat')
 %print flag
 print_flag=0;
-%number of elements in dataset
-N;
 %Input data matrix X
 X=[dataset(:,1),dataset(:,2)];
 %number of clusters C
@@ -12,34 +10,13 @@ C=N_clusters;
 %q = 1 implies hard K-means
 q_hard=1;
 q_soft=1.5;
-%
-lambda=10;
+
 %matrix with outliers(initialize it without outliers)
 O_init=zeros(2,N);
 %Solutions are saved in U (initialize it)
-%U{1}=initU(X,N,C);
-U_init=initU_randomly(N,C);
-
-%threshold to exit the 
-threshold=10^(-3);
+load('Init_variables.mat');
 
 M_init=zeros(2,C);
-
-N_points=30;
-n_outliers=zeros(1,N_points);
-loglambda=logspace(-4,4,N_points);
-for log_lambda_i=1:N_points
-    [M_aux, O_aux, U_aux]=Algorithm_1(M_init,O_init,X,U_init,N,C,loglambda(log_lambda_i),q_hard,threshold,print_flag);
-    n_outliers(1,log_lambda_i)= sum(O_aux(1,:)~=0 & O_aux(2,:)~=0,2);
-end
-display('Initial U was founded')
-figure(10)
-semilogx(loglambda,n_outliers,'-s')
-grid on
-title('Numero de outliers no varrimento no \lambda')
-xlabel('log(\lambda)') % x-axis label
-ylabel('Numero de outliers') % y-axis label
-U_init_star=U_aux;
 
 %threshold to exit the 
 threshold=10^(-6);
@@ -49,15 +26,15 @@ n_outliers_soft=zeros(1,N_points);
 k=1;
 linelambda=linspace(2,20,N_points);
 for lambda_i=1:N_points
-    [M_final_hard{lambda_i}, O_final_hard{lambda_i}, U_final_hard{lambda_i}]=Algorithm_1(M_init,O_init,X,U_init_star,N,C,linelambda(lambda_i),q_hard,threshold,print_flag);
+    [M_final_hard{lambda_i}, O_final_hard{lambda_i}, U_final_hard{lambda_i}]=Algorithm_1(M_init,O_init,X,U_init,N,C,linelambda(lambda_i),q_hard,threshold,print_flag);
     n_outliers_hard(1,lambda_i)= sum(O_final_hard{lambda_i}(1,:)~=0 & O_final_hard{lambda_i}(2,:)~=0,2);
 end
-display('Hard key-means done')
+disp('Hard key-means done')
 for lambda_i=1:N_points
-    [M_final_soft{lambda_i}, O_final_soft{lambda_i}, U_final_soft{lambda_i}]=Algorithm_1(M_init,O_init,X,U_init_star,N,C,linelambda(lambda_i),q_soft,threshold,print_flag);
+    [M_final_soft{lambda_i}, O_final_soft{lambda_i}, U_final_soft{lambda_i}]=Algorithm_1(M_init,O_init,X,U_init,N,C,linelambda(lambda_i),q_soft,threshold,print_flag);
     n_outliers_soft(1,lambda_i)= sum(O_final_soft{lambda_i}(1,:)~=0 & O_final_soft{lambda_i}(2,:)~=0,2);
 end
-display('Soft key-means done')
+disp('Soft key-means done')
 figure(11)
 plot(linelambda,n_outliers_soft,'.-r')
 title('SOFT Key-Means')
@@ -79,22 +56,22 @@ M={M_final_hard{12} M_final_soft{12}};
 O={O_final_hard{12} O_final_soft{12}};
 U={U_final_hard{12} U_final_soft{12}};
 for i=1:1:2
-%norm of the error between the center of mass that we found and the real ones
-[error_mass_center(1),cluster_i(1)]=min(sqrt(sum((M{i}(:,1)-[center_1',center_2',center_3',center_4']).^2, 1)));
-[error_mass_center(2),cluster_i(2)]=min(sqrt(sum((M{i}(:,2)-[center_1',center_2',center_3',center_4']).^2, 1)));
-[error_mass_center(3),cluster_i(3)]=min(sqrt(sum((M{i}(:,3)-[center_1',center_2',center_3',center_4']).^2, 1)));
-[error_mass_center(4),cluster_i(4)]=min(sqrt(sum((M{i}(:,4)-[center_1',center_2',center_3',center_4']).^2, 1)));
+    %norm of the error between the center of mass that we found and the real ones
+    [error_mass_center(1),cluster_i(1)]=min(sqrt(sum((M{i}(:,1)-[center_1',center_2',center_3',center_4']).^2, 1)));
+    [error_mass_center(2),cluster_i(2)]=min(sqrt(sum((M{i}(:,2)-[center_1',center_2',center_3',center_4']).^2, 1)));
+    [error_mass_center(3),cluster_i(3)]=min(sqrt(sum((M{i}(:,3)-[center_1',center_2',center_3',center_4']).^2, 1)));
+    [error_mass_center(4),cluster_i(4)]=min(sqrt(sum((M{i}(:,4)-[center_1',center_2',center_3',center_4']).^2, 1)));
 
-cluster=ones(N,1)*5;
-for j=1:N
-    if O{i}(:,j)==0
-        [~,cluster(j,1)]=max(U{i}(j,:));
-        cluster(j,1)=cluster_i(cluster(j,1));
+    cluster=ones(N,1)*5;
+    for j=1:N
+        if O{i}(:,j)==0
+            [~,cluster(j,1)]=max(U{i}(j,:));
+            cluster(j,1)=cluster_i(cluster(j,1));
+        end
     end
-end
 
-error_mass_center_set{i}=error_mass_center;
-n_outliers_set{i}= sum(O{i}(1,:)~=0 & O{i}(2,:)~=0,2);
-%number of points that are not int the corrected cluster
-errors_clustering_set{i}=length(nonzeros(dataset(:,3)-cluster));
+    error_mass_center_set{i}=error_mass_center;
+    n_outliers_set{i}= sum(O{i}(1,:)~=0 & O{i}(2,:)~=0,2);
+    %number of points that are not int the corrected cluster
+    errors_clustering_set{i}=length(nonzeros(dataset(:,3)-cluster));
 end
